72db42340f49067394482ab372e3ecf2aa0c2262
Mark Barger <mbarger@aphysci.com>
2012-08-17 12:57:24 -0400

    remove binaries.  We'll now be required to rebuild this on every platform.

--- ThirdParty/protobuf-2.4.1/config.h
+++ ThirdParty/protobuf-2.4.1/config.h
@@ -80,7 +80,7 @@
 #define HAVE_UNISTD_H 1
 
 /* Enable classes using zlib compression. */
-/* #undef HAVE_ZLIB */
+#define HAVE_ZLIB 1
 
 /* Define to the sub-directory in which libtool stores uninstalled libraries.
    */
