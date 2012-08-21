03cbca2c58aa43026c4307a35d3a0a0db86febf3
Chris Brundick <cbrundick@aphysci.com>
2012-08-21 10:50:54 -0400

    Empty implementation of virtual destructor

---
+++ src/api/cpp/GravitySubscriber.cpp
@@ -0,0 +1,20 @@
+/*
+ * GravitySubscriber.cpp
+ *
+ *  Created on: Aug 20, 2012
+ *      Author: Chris Brundick
+ */
+
+#include "GravitySubscriber.h"
+
+namespace gravity {
+
+using namespace std;
+
+GravitySubscriber::~GravitySubscriber() {}
+
+}
+
+
+
+
