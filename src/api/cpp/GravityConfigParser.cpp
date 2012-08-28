73793406900f6dee3a1204490dec14d797b7eb9b
Tim Ludwinski <tludwinski@aphysci.com>
2012-08-28 14:39:12 -0500

    Removed Extrainous code

--- src/api/cpp/GravityConfigParser.cpp
+++ src/api/cpp/GravityConfigParser.cpp
@@ -10,16 +10,16 @@ void GravityConfigParser::ParseConfigFile(const char* filename, const char** add
     dictionary* myconfig = iniparser_load(filename);
 
     //Ini parser causes warnings!!!
-    char* protocol = iniparser_getstring(myconfig, "ServiceDirectory:protocol", "tcp");
-    char* interface = iniparser_getstring(myconfig, "ServiceDirectory:interface", "*");
-    char* port = iniparser_getstring(myconfig, "ServiceDirectory:port", "5555");
+    const char* protocol = iniparser_getstring(myconfig, "ServiceDirectory:protocol", "tcp");
+    const char* interface = iniparser_getstring(myconfig, "ServiceDirectory:interface", "*");
+    const char* port = iniparser_getstring(myconfig, "ServiceDirectory:port", "5555");
 
     free(serviceDirectoryUrl);
     serviceDirectoryUrl = (char*) malloc(sizeof(char)*256);
 
     sprintf(serviceDirectoryUrl, "%s://%s:%s", protocol, interface, port);
 
-    char* loglevstr = iniparser_getstring(myconfig, "General:LogLocalLevel", "warning");
+    const char* loglevstr = iniparser_getstring(myconfig, "General:LogLocalLevel", "warning");
     log_local_level = Log::LogStringToLevel(loglevstr);
 
     loglevstr = iniparser_getstring(myconfig, "General:LogNetLevel", "warning");
@@ -27,7 +27,7 @@ void GravityConfigParser::ParseConfigFile(const char* filename, const char** add
 
     while(additional_keys_to_extract != NULL)
     {
-        values[*additional_keys_to_extract] = iniparser_getstring(myconfig, *additional_keys_to_extract, "");
+        values[*additional_keys_to_extract] = string(iniparser_getstring(myconfig, (*additional_keys_to_extract), ""));
         additional_keys_to_extract++;
     }
 
