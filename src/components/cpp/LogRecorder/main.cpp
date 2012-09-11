4467fbb7581c557c1df27454bc3e56c0cde58c6f
tludwinski <tludwinski@aphysci.com>
2012-09-11 14:57:15 -0400

    Fixed for Windows.

--- src/components/cpp/LogRecorder/main.cpp
+++ src/components/cpp/LogRecorder/main.cpp
@@ -2,6 +2,12 @@
 #include <GravityNode.h>
 #include "GravityLogRecorder.h"
 
+#ifdef WIN32
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#define pause() while(true) Sleep(4000000000u);
+#endif
+
 int main()
 {
   using namespace gravity;
