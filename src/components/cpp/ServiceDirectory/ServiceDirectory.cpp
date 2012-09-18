dc8e85a0a1c6d0ddd096805fb91e1107c9cc80b4
tludwinski <tludwinski@aphysci.com>
2012-09-18 14:13:03 -0400

    Removed Debug print for log level.

--- src/components/cpp/ServiceDirectory/ServiceDirectory.cpp
+++ src/components/cpp/ServiceDirectory/ServiceDirectory.cpp
@@ -31,7 +31,6 @@ int main(void)
     parser.ParseConfigFile();
     //gravity::Log::initAndAddFileLogger("ServiceDirectory.log", parser.getLocalLogLevel());
     gravity::Log::initAndAddConsoleLogger(parser.getLocalLogLevel());
-    cout << gravity::Log::LogLevelToString( parser.getLocalLogLevel()) << endl;
     //Not logging to GravityLogRecorder for now because that relies on having a gravity node.
 
     gravity::ServiceDirectory* serviceDirectory = new gravity::ServiceDirectory(parser.getServiceDirectoryUrl().c_str());
