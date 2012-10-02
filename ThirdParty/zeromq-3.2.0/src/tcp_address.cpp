0f8da7faefc7cb36485f3fc8d9e800b7d136a6b6
Mark Barger <mbarger@aphysci.com>
2012-10-02 11:18:05 -0400

    fix zmq to build on windows.  It appears this will be patched, but wasn't patched yet in our version (http://lists.zeromq.org/pipermail/zeromq-dev/2012-July/017674.html)

--- ThirdParty/zeromq-3.2.0/src/tcp_address.cpp
+++ ThirdParty/zeromq-3.2.0/src/tcp_address.cpp
@@ -371,10 +371,10 @@ zmq::tcp_address_t::tcp_address_t (const sockaddr *sa, socklen_t sa_len)
     zmq_assert(sa && sa_len > 0);
 
     memset (&address, 0, sizeof (address));
-    if (sa->sa_family == AF_INET && sa_len >= sizeof (address.ipv4)) {
+    if (sa->sa_family == AF_INET && sa_len >= (int)sizeof (address.ipv4)) {
         memcpy(&address.ipv4, sa, sizeof (address.ipv4));
     }
-    else if (sa->sa_family == AF_INET6 && sa_len >= sizeof (address.ipv6)) {
+    else if (sa->sa_family == AF_INET6 && sa_len >= (int)sizeof (address.ipv6)) {
         memcpy(&address.ipv6, sa, sizeof (address.ipv6));
     }
 }
@@ -577,7 +577,7 @@ int zmq::tcp_address_mask_t::to_string (std::string &addr_)
 
 const bool zmq::tcp_address_mask_t::match_address (const struct sockaddr *ss, const socklen_t ss_len) const
 {
-    zmq_assert (address_mask != -1 && ss != NULL && ss_len >= sizeof(struct sockaddr));
+    zmq_assert (address_mask != -1 && ss != NULL && ss_len >= (int)sizeof(struct sockaddr));
 
     if (ss->sa_family != address.generic.sa_family)
         return false;
