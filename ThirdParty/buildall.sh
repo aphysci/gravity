0a721759ffb22a4b5da1996fc05102ae49389135
Mark Barger <mbarger@aphysci.com>
2012-10-05 13:58:53 -0400

    add usage, force exit on failure, allow specifying clean and disable protobuf java tests since they fail on windows

--- ThirdParty/buildall.sh
+++ ThirdParty/buildall.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
 
+if [ ${#*} -gt 1 ]; then
+    echo "Usage: buildall.sh [clean]"
+    exit 1
+fi
+
 pushd zeromq-3.2.0
-./configure
-make
+./configure || exit 1
+make $@ || exit 1
 popd
 
 pushd protobuf-2.4.1
-./configure
-make
+./configure || exit 1
+make $@ || exit 1
 pushd java
-mvn install
+if [ ${#*} -lt 1 ]; then
+    mvn -DskipTests package || exit 1
+else
+    mvn $@ || exit 1
+fi
 popd
 popd
 
 pushd iniparser
-make
+make $@ || exit 1
 popd
 
 pushd cppdb-trunk
-cmake .
-make
+cmake . || exit 1
+make $@ || exit 1
 popd
 
