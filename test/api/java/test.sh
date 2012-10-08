befa3acdb9b9938221532216d75ff95e050daaad
Mark Barger <mbarger@aphysci.com>
2012-10-08 12:06:49 -0400

    add testall script and make pushd into its own dir before running

--- test/api/java/test.sh
+++ test/api/java/test.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+pushd $SCRIPT_DIR
+
 export PATH=$PATH:../../../ThirdParty/bin:../../../src/components/cpp/bin
 
 ServiceDirectory &
 SDPID=$!
 
 make test
+ret=$?
 
 kill $SDPID
+
+popd
+
+exit $ret
