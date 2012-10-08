d7f9fda25a90fb5b5b931450999ad41030093e26
Mark Barger <mbarger@aphysci.com>
2012-10-08 11:51:50 -0400

    add a second sleep to let SD get started.  Only seems to be needed on windows

--- test/api/java/test.sh
+++ test/api/java/test.sh
@@ -4,6 +4,8 @@ export PATH=$PATH:../../../ThirdParty/bin:../../../src/components/cpp/bin
 
 ServiceDirectory &
 SDPID=$!
+# give the SD a second to start up (only seems to be necessary in windows)
+sleep 1
 
 make test
 
