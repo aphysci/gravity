ab2ab9585975a503af8dfd3320673ff6ecbad228
Mark Barger <mbarger@aphysci.com>
2012-10-17 08:39:17 -0400

    add protobuf jar to lib, and make mvn build only run if mvn is installed.  Added new ThirdParty include dir that will contain the protobuf headers.  Update all Makefiles to use these new locatins for protobuf jar and headers.

--- ThirdParty/buildall.sh
+++ ThirdParty/buildall.sh
@@ -17,13 +17,24 @@ pushd protobuf-2.4.1
 ./configure || exit 1
 make $@ || exit 1
 pushd java
-if [ ${#*} -lt 1 ]; then
-    mvn -DskipTests package || exit 1
+# jar is checked in, so no reason for anyone else to build this.  But just in case, here it is...
+command -v mvn > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+    if [ ${#*} -lt 1 ]; then
+        mvn -DskipTests package || exit 1
+    else
+        mvn $@ || exit 1
+    fi
 else
-    mvn $@ || exit 1
+    echo Maven is not installed, so not building protobuf jar
 fi
 popd
 popd
+
+rm -rf include/*
+find protobuf-2.4.1/src/ -type f -iname "*.h*" -exec cp --parents {} include ";"
+mv include/protobuf-2.4.1/src/google include
+rm -rf include/protobuf-2.4.1
 
 pushd iniparser
 make $@ || exit 1
@@ -38,9 +49,10 @@ fi
 make $@ || exit 1
 popd
 
-rm -rf ./lib/*
+rm -rf ./lib/*.a ./lib/*.so
 find . -path './lib' -prune -o -name *.so -exec cp {} lib \;
 find . -path './lib' -prune -o -name *.a -exec cp {} lib \;
+find . -path './lib' -prune -o -name *.jar -exec cp {} lib \;
 
 rm -rf ./bin/*
 cp protobuf-2.4.1/src/.libs/protoc protobuf-2.4.1/src/.libs/protoc.exe ./bin >& /dev/null
