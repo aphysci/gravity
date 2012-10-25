713c8521d0599d36c6d55620f2be82e769f8b093
Tim Ludwinski <tludwinski@aphysci.com>
2012-10-25 13:40:50 -0400

    Updated Visual Studio build to exit on failure.

--- BuildGravityVS.bat
+++ BuildGravityVS.bat
@@ -10,18 +10,18 @@ mkdir bin
 mkdir lib
 
 cd protobuf-2.4.1\vsprojects
-msbuild libprotobuf.vcxproj /p:Configuration=Release
-msbuild protoc.vcxproj /p:Configuration=Release
+msbuild libprotobuf.vcxproj /p:Configuration=Release || goto build_fail
+msbuild protoc.vcxproj /p:Configuration=Release || goto build_fail
 copy Release\libprotobuf.lib ..\..\lib
 copy Release\protoc.exe ..\..\bin
 
 cd ..\..\zeromq-3.2.0\builds\msvc11
-msbuild msvc11.sln /p:Configuration=Release
+msbuild msvc11.sln /p:Configuration=Release || goto build_fail
 copy ..\..\lib\Win32\libzmq.lib ..\..\..\lib\libzmq.lib
 copy ..\..\bin\Win32\libzmq.dll ..\..\..\bin\libzmq.dll
 
 cd ..\..\..\iniparser\build
-msbuild iniparser.sln /p:Configuration=Release
+msbuild iniparser.sln /p:Configuration=Release || goto build_fail
 Rem lib File from this project is built directly into the lib directory
 
 cd ..\..\..
@@ -30,12 +30,12 @@ copy ThirdParty\pthreads\bin\pthreadVCE2.dll ThirdParty\bin
 
 Rem Build gravity
 cd build\msvs\gravity
-msbuild gravity.sln /p:Configuration=Release
+msbuild gravity.sln /p:Configuration=Release || goto build_fail
 copy Release\gravity.dll ..\..\..\bin
 copy Release\gravity.lib ..\..\..\lib
 
 cd ..\..\..\build\msvs\components\ServiceDirectory
-msbuild ServiceDirectory.sln /p:Configuration=Release
+msbuild ServiceDirectory.sln /p:Configuration=Release || goto build_fail
 copy Release\ServiceDirectory.exe ..\..\..\..\bin
 copy ..\..\..\..\src\components\cpp\ServiceDirectory\ServiceDirectory.ini ..\..\..\..\bin
 cd ..\..\..\..\
@@ -49,3 +49,10 @@ copy src\api\cpp\*.h include
 xcopy /s /q /y ThirdParty\protobuf-2.4.1\src\*.h include
 cd include\google
 cd ..\..
+
+goto end
+
+:build_fail
+@echo Build Failed
+
+:end
