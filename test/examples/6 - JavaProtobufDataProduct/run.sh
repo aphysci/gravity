ee8ad0bf7eb9997b3e4f69500fb4bce6d3af97e8
Dennis Muench <dmuench@aphysci.com>
2013-07-22 17:03:15 -0400

    Added a Lesser General Public License header, which names APS as the rights holder, to all of the makefiles and shell scripts.

--- test/examples/6 - JavaProtobufDataProduct/run.sh
+++ test/examples/6 - JavaProtobufDataProduct/run.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+#** (C) Copyright 2013, Applied Physical Sciences Corp., A General Dynamics Company
+#**
+#** Gravity is free software; you can redistribute it and/or modify
+#** it under the terms of the GNU Lesser General Public License as published by
+#** the Free Software Foundation; either version 3 of the License, or
+#** (at your option) any later version.
+#**
+#** This program is distributed in the hope that it will be useful,
+#** but WITHOUT ANY WARRANTY; without even the implied warranty of
+#** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#** GNU Lesser General Public License for more details.
+#**
+#** You should have received a copy of the GNU Lesser General Public 
+#** License along with this program;
+#** If not, see <http://www.gnu.org/licenses/>.
+#**
+
 
 export PATH=$PATH:$PROTOBUF_INCLUDE_DIR:../../../src/api/java:../../../src/components/cpp/bin
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ZMQ_LIB_DIR:../../../src/api/cpp:../../../src/keyvalue_parser:$ZMQ_LIB_DIR:$PROTOBUF_LIB_DIR
