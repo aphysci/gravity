984827771bd0774cb4e8ef2d6418c286624b5a6b
Mark Barger <mbarger@aphysci.com>
2016-03-17 14:49:15 -0400

    add new example that demonstrates using Domains

---
+++ test/examples/9-Domains/ProtobufDataProductPublisher.cpp
@@ -0,0 +1,73 @@
+/** (C) Copyright 2013, Applied Physical Sciences Corp., A General Dynamics Company
+ **
+ ** Gravity is free software; you can redistribute it and/or modify
+ ** it under the terms of the GNU Lesser General Public License as published by
+ ** the Free Software Foundation; either version 3 of the License, or
+ ** (at your option) any later version.
+ **
+ ** This program is distributed in the hope that it will be useful,
+ ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+ ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ ** GNU Lesser General Public License for more details.
+ **
+ ** You should have received a copy of the GNU Lesser General Public
+ ** License along with this program;
+ ** If not, see <http://www.gnu.org/licenses/>.
+ **
+ */
+
+#include <iostream>
+#include <GravityNode.h>
+#include <GravityLogger.h>
+#include <Utility.h>
+
+#include "../protobuf/BasicCounterDataProduct.pb.h"
+
+int main()
+{
+	using namespace gravity;
+
+	GravityNode gn;
+	//Initialize gravity, giving this node a componentID.
+	GravityReturnCode grc = gn.init("ProtobufGravityComponentID");
+	while (grc != GravityReturnCodes::SUCCESS)
+	{
+	    Log::warning("Unable to connect to ServiceDirectory, will try again in 1 second...");
+	    gravity::sleep(1000);
+	    grc = gn.init("ProtobufGravityComponentID");
+	}
+
+	gn.registerDataProduct(
+							//This identifies the Data Product to the service directory so that others can
+							// subscribe to it.  (See BasicDataProductSubscriber.cpp).
+							"BasicCounterDataProduct",
+							//Assign a transport type to the socket (almost always tcp, unless you are only
+							//using the gravity data product between two processes on the same computer).
+							GravityTransportTypes::TCP);
+
+	bool quit = false; //TODO: set this when you want the program to quit if you need to clean up before exiting.
+	int count = 1;
+	while(!quit)
+	{
+		//Create a data product to send across the network of type "BasicCounterDataProduct".
+		GravityDataProduct counterDataProduct("BasicCounterDataProduct"); //In order to publish, the DataProductID must match one of the registered types.
+
+		//Initialize our message
+		BasicCounterDataProductPB counterDataPB;
+		counterDataPB.set_count(count);
+
+		//Put message into data product
+		counterDataProduct.setData(counterDataPB);
+
+		//Publish the data product.
+		gn.publish(counterDataProduct);
+
+		//Increment count
+		count++;
+		if(count > 50)
+			count = 1;
+
+		//Sleep for 1 second.
+		gravity::sleep(1000);
+	}
+}
---
+++ test/examples/9-Domains/ProtobufDataProductSubscriber.cpp
@@ -0,0 +1,73 @@
+/** (C) Copyright 2013, Applied Physical Sciences Corp., A General Dynamics Company
+ **
+ ** Gravity is free software; you can redistribute it and/or modify
+ ** it under the terms of the GNU Lesser General Public License as published by
+ ** the Free Software Foundation; either version 3 of the License, or
+ ** (at your option) any later version.
+ **
+ ** This program is distributed in the hope that it will be useful,
+ ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+ ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ ** GNU Lesser General Public License for more details.
+ **
+ ** You should have received a copy of the GNU Lesser General Public
+ ** License along with this program;
+ ** If not, see <http://www.gnu.org/licenses/>.
+ **
+ */
+
+#include <iostream>
+#include <GravityNode.h>
+#include <GravityLogger.h>
+#include <GravitySubscriber.h>
+#include <Utility.h>
+
+#include "../protobuf/BasicCounterDataProduct.pb.h"
+
+using namespace gravity;
+
+//Declare class for receiving Published messages.
+class SimpleGravityCounterSubscriber : public GravitySubscriber
+{
+public:
+	virtual void subscriptionFilled(const std::vector< shared_ptr<GravityDataProduct> >& dataProducts);
+};
+
+int main()
+{
+	GravityNode gn;
+	//Initialize gravity, giving this node a componentID.
+	GravityReturnCode grc = gn.init("SimpleGravityComponentID2");
+    while (grc != GravityReturnCodes::SUCCESS)
+    {
+        Log::warning("Unable to connect to ServiceDirectory, will try again in 1 second...");
+        gravity::sleep(1000);
+        grc = gn.init("SimpleGravityComponentID2");
+    }
+
+	//Declare an object of type SimpleGravityCounterSubscriber (this also initilizes the total count to 0).
+	SimpleGravityCounterSubscriber counterSubscriber;
+	//Subscribe a SimpleGravityCounterSubscriber to the counter data product.
+	gn.subscribe("BasicCounterDataProduct", counterSubscriber);
+
+	//Wait for us to exit (Ctrl-C or being killed).
+	gn.waitForExit();
+
+	//Currently this will never be hit because we will have been killed (unfortunately).
+	//But this shouldn't make a difference because the OS should close the socket and free all resources.
+	gn.unsubscribe("BasicCounterDataProduct", counterSubscriber);
+}
+
+void SimpleGravityCounterSubscriber::subscriptionFilled(const std::vector< shared_ptr<GravityDataProduct> >& dataProducts)
+{
+	for(std::vector< shared_ptr<GravityDataProduct> >::const_iterator i = dataProducts.begin();
+			i != dataProducts.end(); i++)
+	{
+		//Get the protobuf object from the message
+		BasicCounterDataProductPB counterDataPB;
+		(*i)->populateMessage(counterDataPB);
+
+		//Process the message
+		Log::warning("Current Count: %d", counterDataPB.count());
+	}
+}
