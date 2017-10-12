d2d8ea9d4e9e907b0f7f73752b3f4218b0437aff
Mark Barger <mbarger@aphysci.com>
2017-10-12 13:26:36 -0400

    fix compile warnings caused by incorrect type on local vars

--- src/api/java/src/cpp/CPPGravitySubscriber.cpp
+++ src/api/java/src/cpp/CPPGravitySubscriber.cpp
@@ -30,14 +30,14 @@ void CPPGravitySubscriber::subscriptionFilled(const std::vector< shared_ptr<Grav
 {
     int* lengths = new int[dataProducts.size()];
     int arrayLength = 0;
-    for (int index = 0; index < dataProducts.size(); index++)
+    for (unsigned int index = 0; index < dataProducts.size(); index++)
     {
         arrayLength += dataProducts[index]->getSize();
         lengths[index] = dataProducts[index]->getSize();
     }
     unsigned char* array = new unsigned char[arrayLength];
     int offset = 0;
-    for (int index = 0; index < dataProducts.size(); index++)
+    for (unsigned int index = 0; index < dataProducts.size(); index++)
     {
         dataProducts[index]->serializeToArray(&array[offset]);
         offset += lengths[index];
