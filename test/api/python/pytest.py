32afb538029dff6cd40ffb13d7039c354e642387
Mark Barger <mbarger@aphysci.com>
2017-10-30 11:56:07 -0400

    add sync test and cleanup code a bit

--- test/api/python/pytest.py
+++ test/api/python/pytest.py
@@ -5,43 +5,50 @@ import gravity
 from gravity import GravityNode, GravityDataProduct, gravity, GravitySubscriber, GravityRequestor, GravityServiceProvider, Log
 from PythonTestPB_pb2 import PythonTestPB
 
-testCount = 0
-
+###
+### Classes for various handlers
+###
 class MySubscriber(GravitySubscriber):
+    def __init__(self):
+        super(MySubscriber, self).__init__()
+        self.subCount = 0
+
     def subscriptionFilled(self, dataProducts):
         testPB = PythonTestPB()
         for gdp in dataProducts:
             gdp.populateMessage(testPB)
             Log.message("received counter with value = "+str(testPB.count))
-            global testCount
-            testCount = testPB.count
+            self.subCount = testPB.count
 
 class MyRequestHandler(GravityRequestor):
     def __init__(self, gravityNode):
         super(MyRequestHandler, self).__init__()
         self.gravityNode = gravityNode 
+        self.reqCount = 0
 
     def requestFilled(self, serviceID, requestID, response):
         testPB = PythonTestPB()
         response.populateMessage(testPB)
         Log.message("made it to request filled with request GDP ID = "+response.getDataProductID() +" and count = " + str(testPB.count))
-        global testCount
-        testCount = testPB.count
-        if testCount < 5:
+        self.reqCount = testPB.count
+        if self.reqCount < 5:
             gdp = GravityDataProduct("ServiceRequest")
             gdp.setData(testPB)
             self.gravityNode.request("ServiceTest", gdp, self)
             
-
 class TestProvider(GravityServiceProvider):
     def request(self, serviceID, dataProduct):
-        testPB = PythonTestPB()
-        dataProduct.populateMessage(testPB)
-        testPB.count += 1
+        reqPB = PythonTestPB()
+        dataProduct.populateMessage(reqPB)
+        reqPB.count += 1
         gdp = GravityDataProduct("ServiceTestResponse")
-        gdp.setData(testPB)
+        gdp.setData(reqPB)
         return gdp
 
+
+###
+### Test functions
+###
 def testPubSub(gravityNode):
     gravityNode.registerDataProduct("PubTest", gravity.TCP)
     mySub = MySubscriber()
@@ -51,14 +58,13 @@ def testPubSub(gravityNode):
     pubPB.count = 0
     pubPB.message = ""
     gdp = GravityDataProduct("PubTest")
-    global testCount
-    while testCount < 5 and pubPB.count < 10:
+    while mySub.subCount < 5 and pubPB.count < 10:
         pubPB.count += 1
         gdp.setData(pubPB)
         gravityNode.publish(gdp)
         time.sleep(1)
         
-    if testCount < 5:
+    if mySub.subCount < 5:
         Log.critical("Pub/Sub failed")
         return 1
     return 0
@@ -74,16 +80,30 @@ def testService(gravityNode):
     gdp.setData(testPB)
     gravityNode.request("ServiceTest", gdp, myReq)
 
-    global testCount
-    testCount = 0
+    # test async
     loopCount = 0
-    while testCount < 5 and loopCount < 5:
+    while myReq.reqCount < 5 and loopCount < 5:
         loopCount += 1
         time.sleep(1)
-        
-    if testCount < 5:
-        Log.critical("Service Request failed")
+         
+    if myReq.reqCount < 5:
+        Log.critical("Asynchronous Service Request failed, expected {} on counter, but was {}".format(5, myReq.reqCount))
         return 1
+    
+    # test sync
+    testPB.count = 0
+    gdp.setData(testPB)
+    responsePB = PythonTestPB()
+    for i in range(0, 5):
+        responseGDP = gravityNode.request("ServiceTest", gdp)
+        responseGDP.populateMessage(responsePB)
+        if responsePB.count != testPB.count+1:
+            Log.critical("Incorrect return value, got {} but expected {}".format(responsePB.count, testPB.count+1))
+            return 1
+        else:
+            Log.message("Received return value {} on synchronous request".format(responsePB.count))
+        testPB.count += 1
+        gdp.setData(testPB)
     
 def main():
     gravityNode = GravityNode()
