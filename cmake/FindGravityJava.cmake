06183fc5d546755bab25aba91f81e3cb0d089406
Justin Borodinsky <jborodinsky@aphysci.com>
2020-02-18 14:07:54 -0500

    Various CMake fixes and CMake build of first example

--- cmake/FindGravityJava.cmake
+++ cmake/FindGravityJava.cmake
@@ -1,10 +1,5 @@
 
-
-if (JAVA_HOME)
-    find_package(Java COMPONENTS Development PATHS "${JAVA_HOME}")
-else()
-    find_package(Java COMPONENTS Development)
-endif()
+find_package(Java COMPONENTS Development)
 
 include(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(GravityJava REQUIRED_VARS Java_FOUND)
