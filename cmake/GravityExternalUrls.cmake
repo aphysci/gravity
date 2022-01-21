586de2ddb6c57070f85b283fa00bd09cbdeac6e5
cbrundick <cbrundick@aphysci.com>
2022-01-21 13:12:47 -0800

    Initial attempt to add spdlog to cmake build

--- cmake/GravityExternalUrls.cmake
+++ cmake/GravityExternalUrls.cmake
@@ -5,12 +5,16 @@
 ## Protobuf version requirements
 # Versions greater than 3.5.1 check for a __cplusplus value that's incorrect and too low in VS2013.  So this is the latest protobuf version that will still build with 2013.
 # A Cmake build is required so the minimum version you can use is 3.4.1
-set(protobuf_url "https://github.com/protocolbuffers/protobuf/archive/v3.5.1.zip")
+set(protobuf_url "https://github.com/protocolbuffers/protobuf/archive/v3.6.1.zip")
 
 
 ## ZeroMQ version requirements
 # The minimum version you can use externally is 4.2.0
 set(libzmq_url "https://github.com/zeromq/libzmq/archive/v4.3.2.zip")
+
+## spdlog version requirements
+set(spdlog_url "https://github.com/gabime/spdlog/archive/refs/heads/v1.x.zip")
+#set(spdlog_url "https://github.com/gabime/spdlog/archive/refs/tags/v1.9.2.zip")
 
 set(boost_url "https://dl.bintray.com/boostorg/release/1.72.0/source/boost_1_72_0.zip")
 
