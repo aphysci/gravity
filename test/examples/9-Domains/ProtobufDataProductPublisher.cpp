cade82fb4ebddb7ab20081c0d193a04f119b2d28
APS VM <flinker@ubuntu.(none)>
2025-05-30 12:03:43 -0700

    All C++ examples now work with CMake (example 10 is broken for other reasons)

--- test/examples/9-Domains/ProtobufDataProductPublisher.cpp
+++ test/examples/9-Domains/ProtobufDataProductPublisher.cpp
@@ -19,9 +19,10 @@
 #include <iostream>
 #include <GravityNode.h>
 #include <GravityLogger.h>
+#include <spdlog/spdlog.h>
 #include <Utility.h>
 
-#include "../protobuf/BasicCounterDataProduct.pb.h"
+#include "BasicCounterDataProduct.pb.h"
 
 int main()
 {
