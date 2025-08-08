0c99335a84b30a45b797183d0b00e9e696c5d126
teste_static <a1strauch@yahoo.com>
2025-08-08 11:06:18 -0400

    pubsub runnable example

--- test/examples/15-RustBasicDataProduct/build.rs
+++ test/examples/15-RustBasicDataProduct/build.rs
@@ -4,15 +4,22 @@ use std::path::Path;
 fn main() {
 
     let dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
+    
     let root = Path::new(&dir);
     let path = root.parent().unwrap();
     let mut path = path.to_str().unwrap().to_string();
     path.push_str("/protobuf");
-
+    let mut file = path.clone();
+    file.push_str("/BasicCounterDataProduct.proto");
 
     protobuf_codegen::Codegen::new()
+        // .protoc_path(&protoc_bin_vendored::protoc_bin_path().unwrap())
+        // .protoc()
         .pure()
-        .protoc()
         .include(path)
-        .input("BasicCounterDataProduct.proto");
+        .input(file)
+        .cargo_out_dir("protobuf")
+        .run_from_script();
+
+   
 }
