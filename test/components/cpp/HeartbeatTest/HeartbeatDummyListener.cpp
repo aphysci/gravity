77743d3791b510c3f55405f8aa073622a83e4431
Anson Strauch <a1strauch@yahoo.com>
2026-01-22 10:46:20 -0500

    remove final unistd

--- test/components/cpp/HeartbeatTest/HeartbeatDummyListener.cpp
+++ test/components/cpp/HeartbeatTest/HeartbeatDummyListener.cpp
@@ -27,12 +27,6 @@
 #include <iostream>
 #include <GravityNode.h>
 
-#ifdef WIN32
-#include <Windows.h>
-#define sleep Sleep
-#else
-#include <unistd.h>
-#endif
 
 using namespace std;
 using namespace gravity;
